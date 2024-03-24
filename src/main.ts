76358f22de1ca5f2f2efb5834f30e0a42ceeb536
huterguier <>
2024-03-24 18:14:31 +0100

    init

---
+++ src/main.ts
@@ -0,0 +1,75 @@
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+
+// Remember to rename these classes and interfaces!
+
+interface MehrmaidSettings {
+	mySetting: string;
+}
+
+const DEFAULT_SETTINGS: MehrmaidSettings = {
+	mySetting: 'default'
+}
+
+export default class Mehrmaid extends Plugin {
+	settings: MehrmaidSettings;
+	
+	async onload() {
+		await this.loadSettings();
+
+		this.addSettingTab(new SampleSettingTab(this.app, this));
+	}
+
+	onunload() {
+
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+}
+
+class SampleModal extends Modal {
+	constructor(app: App) {
+		super(app);
+	}
+
+	onOpen() {
+		const {contentEl} = this;
+		contentEl.setText('Woah!');
+	}
+
+	onClose() {
+		const {contentEl} = this;
+		contentEl.empty();
+	}
+}
+
+class SampleSettingTab extends PluginSettingTab {
+	plugin: Mehrmaid;
+
+	constructor(app: App, plugin: Mehrmaid) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const {containerEl} = this;
+
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName('Setting #1')
+			.setDesc('It\'s a secret')
+			.addText(text => text
+				.setPlaceholder('Enter your secret')
+				.setValue(this.plugin.settings.mySetting)
+				.onChange(async (value) => {
+					this.plugin.settings.mySetting = value;
+					await this.plugin.saveSettings();
+				}));
+	}
+}
